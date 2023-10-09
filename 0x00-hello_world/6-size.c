606bc094a2b5c2c1dceadba17175f21ed8d8c8b7
Leila <inezaleilla@gmail.com>
2023-10-09 14:10:17 -0700

    print sizeoff

---
+++ 0x00-hello_world/6-size.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+
+/**
+ * main - main function
+ *
+ * Return: 0 on success
+ */
+
+int main (void)
+{
+	printf("Size of a char: %lu byte(s)", sizeof(char));
+	printf("Size of int: %lu bytes(s)", sizeof(int));
+	printf("Size of a long int: %lu byte(s)", sizeof(long int));
+	printf("Size of a long long int: %lu byte(s)", sizeof(long long int));
+	printf("Size of a float: %lu bytes(s)", sizeof(float));
+	return (0);
+}
