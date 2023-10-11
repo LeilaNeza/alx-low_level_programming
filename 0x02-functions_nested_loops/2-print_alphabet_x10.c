d91ef9a8ae186defb0d0805aaf9f2a9571d16ee2
Leila <inezaleilla@gmail.com>
2023-10-11 04:39:08 -0700

    first trial to print alphabet 10 times

---
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -0,0 +1,16 @@
+#include "main.h"
+
+/**
+ * print_alphabet_x10 - prints the alphabet 10 times
+ * print_alphabet - calling function to print the alphabet
+ */
+
+void print_alphabet_x10(void)
+{
+	int i;
+
+	for (i = 0; i < 10; i++)
+	{
+		print_alphabet();
+	}
+}
