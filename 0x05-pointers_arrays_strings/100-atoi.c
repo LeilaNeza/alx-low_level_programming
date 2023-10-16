daec7d32aa84b444a07625940238a44e5c39b8c8
Leila <inezaleilla@gmail.com>
2023-10-16 12:08:27 -0700

    pointers

--- 0x05-pointers_arrays_strings/100-atoi.c
+++ 0x05-pointers_arrays_strings/100-atoi.c
@@ -28,10 +28,11 @@ int _atoi(char *s)
 		if (s[i] >= '0' && s[i] <= '9')
 		{
 			digit = s[i] - '0';
+			
 			if (d % 2)
-				digit = -digit;
-				n = n * 10 + digit;
-				f = 1;
+				digit =- digit;
+			n = n * 10 + digit;
+			f = 1;
 			if (s[i + 1] < '0' || s[i + 1] > '9')
 				break;
 				f = 0;
